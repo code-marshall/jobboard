23eed5087b551b3a0f309ce692a59bc2f2b5543d
Ravikant <ravikantpandey88@gmail.com>
2025-12-23 19:39:16 +0530

    Auto-commit before deploy: 2025-12-23 19:39:16

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './components/auth/AuthContext';
 import ProtectedRoute from './components/auth/ProtectedRoute';
 import HomePage from './components/pages/HomePage';
